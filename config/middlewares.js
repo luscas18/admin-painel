1a882875fc7ccbda1167ed86fc31449b29052624
Lucas Vaz <lucas.vaz@appmoove.com.br>
2022-07-01 12:21:50 -0300

    Changes to my-project noted 2

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,5 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -9,4 +8,19 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          // Enable the download of the Monaco editor
+          // from cdn.jsdelivr.net.
+          "script-src": ["'self'", "cdn.jsdelivr.net", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
+
