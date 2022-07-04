967b336c31bf2922ccc17e3209d962053a24970f
Lucas Vaz <lucas.vaz@appmoove.com.br>
2022-07-04 16:56:23 -0300

    Changes to my-project noted 11

--- config/middlewares.js
+++ config/middlewares.js
@@ -17,6 +17,21 @@ module.exports = [
           // Enable the download of the Monaco editor
           // from cdn.jsdelivr.net.
           "script-src": ["'self'", "cdn.jsdelivr.net", "blob:"],
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'yourBucketName.s3.yourRegion.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'yourBucketName.s3.yourRegion.amazonaws.com',
+          ],
           upgradeInsecureRequests: null,
         },
       },
--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,36 @@
 module.exports = ({ env }) => ({
-    //...
-    graphql: {
-      enabled: true,
-      config: {
-        playgroundAlways: false,
-        defaultLimit: 10,
-        maxLimit: 20,
-        apolloServer: {
-          tracing: true,
-        }
+  //...
+  graphql: {
+    enabled: true,
+    config: {
+      playgroundAlways: false,
+      defaultLimit: 10,
+      maxLimit: 20,
+      apolloServer: {
+        tracing: true,
       }
+    }
+  },
+  'import-export-entries': {
+    enabled: true,
+  },
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
-    'import-export-entries': {
-      enabled: true,
-    },
-    //...
-  });
+  },
+  //...
+});
