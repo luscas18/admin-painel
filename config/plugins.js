3ec51987821915afd31c105f96f3663fc7ed2d6c
Lucas Vaz <lucas.vaz@appmoove.com.br>
2022-07-04 10:00:10 -0300

    Changes to my-project noted 8

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,16 @@
 module.exports = ({ env }) => ({
     //...
+    graphql: {
+      enabled: true,
+      config: {
+        playgroundAlways: false,
+        defaultLimit: 10,
+        maxLimit: 20,
+        apolloServer: {
+          tracing: true,
+        }
+      }
+    },
     'import-export-entries': {
       enabled: true,
     },
