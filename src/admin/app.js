ed1f117860a71fc31f1dc0079e37a5cb278b912a
Lucas Vaz <lucas.vaz@appmoove.com.br>
2022-07-20 16:53:10 -0300

    Changes to my-project noted

---
+++ src/admin/app.js
@@ -0,0 +1,15 @@
+
+export default {
+    config: {
+        locales: ['pt-Br'],
+        translations: {
+            'pt-Br': {
+                Users: 'Utilisateurs',
+                City: 'CITY (Brasil)',
+                // Customize the label of the Content Manager table.
+                Id: 'ID brasil',
+            },
+        },
+    },
+    bootstrap() { },
+};
